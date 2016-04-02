011500b256c91f71d01a4e58392063dba0065f89
wbombardellis <wbombardellis@gitlab.tubit.tu-berlin.de>
2016-04-02 15:15:28 -0300

    First commit: Files from file:///home/william/2016-1/Compiladores/Trabalho/schnorr-compil-2016-1-241ea79ef448/include file:///home/william/2016-1/Compiladores/Trabalho/schnorr-compil-2016-1-241ea79ef448/src file:///home/william/2016-1/Compiladores/Trabalho/schnorr-compil-2016-1-241ea79ef448/tests file:///home/william/2016-1/Compiladores/Trabalho/schnorr-compil-2016-1-241ea79ef448/CMakeLists.txt file:///home/william/2016-1/Compiladores/Trabalho/schnorr-compil-2016-1-241ea79ef448/parser.y file:///home/william/2016-1/Compiladores/Trabalho/schnorr-compil-2016-1-241ea79ef448/README file:///home/william/2016-1/Compiladores/Trabalho/schnorr-compil-2016-1-241ea79ef448/scanner.l

---
+++ src/cc_misc.c
@@ -0,0 +1,29 @@
+#include "cc_misc.h"
+
+int comp_get_line_number (void)
+{
+  //implemente esta função
+  return -1;
+}
+
+void yyerror (char const *mensagem)
+{
+  fprintf (stderr, "%s\n", mensagem); //altere para que apareça a linha
+}
+
+void main_init (int argc, char **argv)
+{
+  //implemente esta função com rotinas de inicialização, se necessário
+}
+
+void main_finalize (void)
+{
+  //implemente esta função com rotinas de inicialização, se necessário
+}
+
+void comp_print_table (void)
+{
+  //para cada entrada na tabela de símbolos
+  //Etapa 1: chame a função cc_dict_etapa_1_print_entrada
+  //implemente esta função
+}
